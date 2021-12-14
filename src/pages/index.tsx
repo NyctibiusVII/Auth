24ffdacc454ef9c52a57776c5c334c35ac36bd43
NyctibiusVII <matheus.dev.07@gmail.com>
2021-12-14 10:53:20 -0300

    Config initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+
+import Head  from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Head><title>Início | Auth</title></Head>
+
+            <main>
+                <h1>Hello Word</h1>
+            </main>
+        </>
+    )
+}
+export default Home
